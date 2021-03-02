acb6ad4738b20ff199dbbc55de0df1820a1ffa83
pradip21 <p2k.das@gmail.com>
2021-03-02 12:34:32 +0530

    First Java Program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+Public Class HelloWorld {
+
+    Public static void main(string[] args) {
+        // Prints "Hello. World" to the terminal window.
+        system.out.println("Hello, World");
+    }
+}        
